b8a48ce3bbb4bdca634601b09aab99be34e7903d
Marcos Martins <marcos_36ma@hotmail.com>
2023-07-26 20:08:04 -0300

    Testes do fluxo de clientes

--- users/src/test/java/com/agendify/users/ClienteTest.java
+++ users/src/test/java/com/agendify/users/ClienteTest.java
@@ -1,12 +1,21 @@
 package com.agendify.users;
 
+import com.agendify.domain.mappers.ClienteMapper;
+import com.agendify.domain.records.Cliente;
+import com.agendify.domain.repositories.ClienteRepository;
 import com.agendify.users.config.TestsConfig;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = TestsConfig.class)
@@ -16,9 +25,51 @@ class ClienteTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    ClienteMapper clienteMapper;
+
     @Test
     void getClienteTest() throws Exception {
-        mockMvc.perform(get("/123213123")).andExpect(status().is2xxSuccessful());
+        Cliente cliente = createClienteData();
+        mockMvc.perform(get("/cliente/" + cliente.id())).andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void createClienteTest() throws Exception {
+        Cliente cliente = clienteMapper.fromEntity(buildClienteEntity());
+
+        mockMvc.perform(
+                        post("/cliente")
+                                .content(parseObjectToJson(cliente))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                ).andExpect(status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nome").value(cliente.nome()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(cliente.email()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value(cliente.cpf()));
+    }
+
+    private String parseObjectToJson(Cliente cliente) throws JsonProcessingException {
+        return new ObjectMapper()
+                .writer()
+                .withDefaultPrettyPrinter()
+                .writeValueAsString(cliente);
+    }
+
+    private Cliente createClienteData() {
+        com.agendify.domain.entities.Cliente cliente = clienteRepository.saveAndFlush(buildClienteEntity());
+        return clienteMapper.fromEntity(cliente);
+    }
+
+    private com.agendify.domain.entities.Cliente buildClienteEntity() {
+        return com.agendify.domain.entities.Cliente.builder()
+                .nome("Fulano Teste")
+                .cpf("1092301293")
+                .email("fulanoHost")
+                .build();
     }
 
 }
--- users/src/test/java/com/agendify/users/config/TestsConfig.java
+++ users/src/test/java/com/agendify/users/config/TestsConfig.java
@@ -2,10 +2,16 @@ package com.agendify.users.config;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @SpringBootConfiguration
 @EnableAutoConfiguration
+@EnableJpaRepositories("com.agendify.domain.repositories")
+@EntityScan("com.agendify.domain.entities")
+@ComponentScan({"com.agendify.domain", "com.agendify.users"})
 public class TestsConfig {
 }
