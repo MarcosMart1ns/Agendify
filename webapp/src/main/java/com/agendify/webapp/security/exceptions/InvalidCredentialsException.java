cbb963ea1eaeaff107e33065ec3727aafa0947e0
Marcos Martins <marcos_36ma@hotmail.com>
2023-08-08 20:04:41 -0300

    Tratamento de erros so solicitar token no login

--- webapp/src/main/java/com/agendify/webapp/security/exceptions/InvalidCredentialsException.java
+++ webapp/src/main/java/com/agendify/webapp/security/exceptions/InvalidCredentialsException.java
@@ -1,5 +1,9 @@
 package com.agendify.webapp.security.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code= HttpStatus.UNAUTHORIZED)
 public class InvalidCredentialsException extends Exception{
 
     public InvalidCredentialsException(String message) {
--- webapp/src/main/java/com/agendify/webapp/security/service/AuthService.java
+++ webapp/src/main/java/com/agendify/webapp/security/service/AuthService.java
@@ -3,10 +3,12 @@ package com.agendify.webapp.security.service;
 import com.agendify.domain.entities.Usuario;
 import com.agendify.domain.repositories.ClienteRepository;
 import com.agendify.webapp.security.exceptions.InvalidCredentialsException;
-import com.agendify.webapp.security.exceptions.InvalidTokenException;
+import com.agendify.webapp.security.exceptions.RequestTokenException;
 import com.agendify.webapp.security.records.AuthRequest;
 import com.agendify.webapp.security.records.AuthResponse;
 import com.agendify.webapp.security.records.KeycloakTokenResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -17,10 +19,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
 
 @Service
 public class AuthService {
+
+    Logger logger = LoggerFactory.getLogger(AuthService.class);
 
     @Autowired
     private ClienteRepository usuarioRepository;
@@ -38,7 +44,10 @@ public class AuthService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    public AuthResponse handleAuthRequest(AuthRequest authRequest) throws InvalidCredentialsException, InvalidTokenException {
+    private final String EMAIL_SENHA_INVALIDOS = "Email ou senha incorretos, verique os dados informados e tente novamente";
+
+    public AuthResponse handleAuthRequest(AuthRequest authRequest) throws InvalidCredentialsException, RequestTokenException {
+        logger.trace(String.format("Verificando dados de login do usuário %s", authRequest.email()));
 
         Usuario usuario = usuarioRepository.findByEmail(authRequest.email());
 
@@ -50,18 +59,21 @@ public class AuthService {
     }
 
     private void checkPassword(AuthRequest authRequest, Usuario usuario) throws InvalidCredentialsException {
+        logger.trace("Verificando senha fornecida");
+
         if (usuario == null) {
-            throw new InvalidCredentialsException("Wrong email or password");
+            throw new InvalidCredentialsException(EMAIL_SENHA_INVALIDOS);
         }
 
         boolean isPasswordValid = passwordEncoder.matches(authRequest.password(), usuario.getSenha());
 
         if (!isPasswordValid) {
-            throw new InvalidCredentialsException("Wrong email or password");
+            throw new InvalidCredentialsException(EMAIL_SENHA_INVALIDOS);
         }
     }
 
-    private String getToken() throws InvalidTokenException {
+    private String getToken() throws RequestTokenException {
+        logger.info("Solicitando novo token ao Keycloak");
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -72,13 +84,17 @@ public class AuthService {
         map.add("grant_type", "client_credentials");
 
         HttpEntity<Object> request = new HttpEntity<>(map, headers);
+        ResponseEntity<KeycloakTokenResponse> response;
 
-        ResponseEntity<KeycloakTokenResponse> response = restTemplate.postForEntity(verifyTokenUrl, request, KeycloakTokenResponse.class);
+        try {
 
-        if (response.getBody() == null) {
-            throw new InvalidTokenException("Erro ao obter access token, resposta invalida do keycloak");
+            response = restTemplate.postForEntity(verifyTokenUrl, request, KeycloakTokenResponse.class);
+            return response.getBody().access_token();
+
+        } catch (RestClientException e) {
+            logger.error("Erro ao solicitar token ao keycloak", e);
+
+            throw new RequestTokenException("Erro ao obter ao token de acesso");
         }
-
-        return response.getBody().access_token();
     }
 }
