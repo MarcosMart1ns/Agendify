211677e9d6560303c249ce9b2de215e0b35e9edf
Marcos Martins <marcos_36ma@hotmail.com>
2023-08-09 22:41:35 -0300

    Estrutura nav-bar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavBarLinkComponent } from './components/nav-bar-link/nav-bar-link.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavBarLinkComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/nav-bar-link/nav-bar-link.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-link',
+  templateUrl: './nav-bar-link.component.html',
+  styleUrls: ['./nav-bar-link.component.css']
+})
+export class NavBarLinkComponent {
+  @Input() buttonName :string = "defaultName";
+}
