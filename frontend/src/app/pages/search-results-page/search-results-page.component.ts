1c799783ef7069d419b876908dd7cacc0f06b4b2
Marcos Martins <marcos_36ma@hotmail.com>
2023-10-10 19:20:31 -0300

    Criação de redirectionamento para a tela de resultado de busca

--- frontend/src/app/pages/search-results-page/search-results-page.component.ts
+++ frontend/src/app/pages/search-results-page/search-results-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {query} from "@angular/animations";
 
 @Component({
   selector: 'app-search-results-page',
   templateUrl: './search-results-page.component.html',
   styleUrls: ['./search-results-page.component.css']
 })
-export class SearchResultsPageComponent {
+export class SearchResultsPageComponent implements OnInit {
 
+  queryText!: string
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let queryParam = params.get('query')
+      if (queryParam) {
+        this.queryText = queryParam;
+      }
+    })
+  }
+
+  protected readonly query = query;
 }
